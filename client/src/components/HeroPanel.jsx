9285ae4fe7e53e4462f449e2337c882627490a66
Ayah Assaad <ayah.assaad@icloud.com>
2026-09-07 10:21:53 +0000

    style: remove hero description text under the SipLog title

--- client/src/components/HeroPanel.jsx
+++ client/src/components/HeroPanel.jsx
@@ -6,10 +6,6 @@ function HeroPanel() {
       <div className="hero-copy">
         <RoseGlassLogo />
         <h1>SipLog</h1>
-        <p className="hero-text">
-          Build your own tasting timeline, collect bottles you loved, and make
-          every sip feel like part of your story.
-        </p>
       </div>
     </header>
   );
